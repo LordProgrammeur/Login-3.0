91aef5ca46a4ca2c31f69bda3dc37e0fedfac6c2
LordProgrammeur <david91ortiz14@gmail.com>
2024-08-20 11:40:00 -0500

    actualizacion alertas

---
+++ views/resultado.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Resultado</title>
+</head>
+<body>
+    <p><?php echo "Se te ha enviado un correo de recuperación"; ?></p>
+    <a href="index.php?controller=users&action=mostrarLogin">Iniciar Sesión</a>
+</body>
+</html>
